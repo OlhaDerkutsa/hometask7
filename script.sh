dc9b4bdd606d3da2fb96772e5b44d612513eda49
OlhaDerkutsa <58556274+OlhaDerkutsa@users.noreply.github.com>
2021-12-13 13:25:40 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,8 @@
-#Port forwading
-ssh -D 1080 -p 3722 root@yoko.ukrtux.com
+# SOCKS proxy over SSH tunnel
+ssh -D 1080 -N -p3922 root@yoko.ukrtux.com
 
-#Reverse port forwarding
-ssh -p 3722 -R 13722:localhost:22 root@yoko.ukrtux.com
+# Reverse proxy in  local machine
+ssh -R 13922:localhost:22 -p 3922 root@yoko.ukrtux.com
+
+# SSH back to my machine via reverse proxy  on remote machine
+ssh -p 13922 vagrant@yoko.ukrtux.com
